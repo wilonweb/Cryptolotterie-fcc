fbb3813f99dfae9e7e9528c3888575ba05322da6
NinjaTurtles-cloud <wilfried.delgrange1987@gmail.com>
2022-12-14 15:55:21 +0100

    transactionError

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Cryptolotterie-fcc
+
+Ou dois aleer les const de helper-hardhat-config ?
+deployer-raffle.js
+
+C'est quoi le VRF ? ( Coordinator, ... )
+
+C'est quoi le GasLane ?
+
+C'est le le EntranceFee ?
+
+C'est quoi le Gwei ?
+
+C'est quoi un Mock ?
+
+C'est quoi la const VRF_SUB_FUND_AMOUNT ?
+
+C'est quoi le KeyHash ( testNet ) ?
+La clé publique utilisé dans le parametre GasLane que l'on trouve dans helper-hardhat-confing.js
+https://docs.chain.link/vrf/v2/subscription/examples/get-a-random-number/
+
+C'est quoi le Subscription ID ?
+https://vrf.chain.link/
+
+C'est quoi l'intervalle ?
+C'est le nombre de seconde
+
+C'est Quoi ? ( CF constructor Raffle.sol )
+
+-   vrfCoordinatorV2,
+-   uint256 entranceFee,
+-   bytes32 gaslane,
+-   uint64 subscriptionId,
+-   uint32 callbackGasLimit,
+-   uint256 interval
--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,27 +1,54 @@
-const { network } = require("hardhat")
-const { developmentChains } = require("../helper-hardahat-config")
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../helper-hardhat-config")
 
-<<<<<<< HEAD
-module.exports = async function ({ getNamedAccounts, deployments}) {
-    const {deploy, log} = deployments
-    const {deployer} = await getNamedAccounts()
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("30")
 
-    /*if (developmentChains.includes(network.name)) {
-
-    }*/
-=======
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
->>>>>>> e71d020ea4c2b2dfd9ba54de5f2efe3594d3d9dd
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address, subscriptionId
 
-    // if (developmentChains.includes(network.name)) {
-    // }
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait(1)
+        subscriptionId = transactionReceipt.events[0].args.subId
+        // Fund the scubscription
+        // Usually you'd need the link token on a real network
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
+    }
 
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    const interval = networkConfig[chainId]["interval"]
+
+    const args = [
+        vrfCoordinatorV2Address,
+        entranceFee,
+        gasLane,
+        subscriptionId,
+        callbackGasLimit,
+        interval,
+    ]
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: [],
         log: true,
         waitConfirmation: network.config.blockConfirmation || 1,
     })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying....")
+        await verify(raflle.adress, args)
+    }
+    log("-----------------")
 }
+
+module.exports.tags = ["all", "raffle"]
